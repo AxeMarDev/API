b9f9eaa4f1bccb81ee076ce99dfa5749c377eb5b
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-04-27 17:42:50 -0500

    Added a struct to represent a person

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+// Person : upper case members can be seen outside the file
+type Person struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Lastname string `json:"lastname"`
+	team     int    `json:"team"`
+}
+
 func main() {
 
 }
