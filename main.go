03c4aeeb3a1eb4054306739193d61ef5b43b5b78
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-04-27 18:09:28 -0500

    added a post request

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
+//https://pkg.go.dev/github.com/gin-gonic/gin#Context.BindJSON for documentation
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 // Person : upper case members can be seen outside the file
 type Person struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	Lastname string `json:"lastname"`
-	team     int    `json:"team"`
+	ID        string `json:"id"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Team      int    `json:"team"`
+}
+
+var People = []Person{
+	{ID: "1", Firstname: "Axell", Lastname: "Martinez", Team: 1},
+	{ID: "2", Firstname: "Kaycee", Lastname: "Menchaca", Team: 1},
+	{ID: "3", Firstname: "Andrik", Lastname: "Martinez", Team: 2},
+}
+
+// this is a function that will get routed too when a GET request to route /People
+func getPeople(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, People)
+}
+
+func addPerson(c *gin.Context) {
+	var newPerson Person
+
+	if err := c.BindJSON(&newPerson); err != nil {
+		return
+	}
+	People = append(People, newPerson)
+	c.IndentedJSON(http.StatusCreated, newPerson)
+
 }
 
 func main() {
-
+	router := gin.Default()
+	router.GET("/people", getPeople)  // to request run 'curl http://localhost:8080/people' on termial
+	router.POST("/people", addPerson) // to request run 'curl http://localhost:8080/people' on termial
+	router.Run("localhost:8080")
 }
